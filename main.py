52641cdf302482e3c828a79a13c2f2c700774e5e
XaviRN <xavirn.me@gmail.com>
2024-03-29 14:45:13 +0100

    feat: new steppermotor class and main logic

--- main.py
+++ main.py
@@ -1,32 +1,16 @@
-import machine
 import utime
 from src.steppermotor import StepperMotor
 
-# Define constants
-STEPS_PER_REV = 200
-
-# The pins used are 1,2,3,4 
-# Connected to L298N Motor Driver In1, In2, In3, In4 
-# Pins entered in sequence 1-2-3-4 for proper step sequencing
-stepper = StepperMotor(STEPS_PER_REV, 1, 2, 3, 4)
-
-# Analog pin for the speed control potentiometer
-speed_control = machine.ADC(26)  # ADC on GP26
-
-def main():
+step_pin = 17
+dir_pin = 16
+stepper = StepperMotor(dir_pin, step_pin)
+ 
+def loop():
     while True:
-        # Read the sensor value
-        sensor_reading = speed_control.read_u16()
-
-        # Map it to a range from 0 to 100
-        motor_speed = sensor_reading / 65535 * 100
-
-        # Set the motor speed
-        if motor_speed > 0:
-            stepper.set_speed(motor_speed)
-            # Step 1/100 of a revolution
-            stepper.step(STEPS_PER_REV / 100)
-        utime.sleep(0.01)  # Small delay to prevent overwhelming the CPU
-
-if __name__ == "__main__":
-    main()
+        stepper.move(600, 2000, 5)  # 2 revolutions forward
+        utime.sleep(1)
+        stepper.move(-600, 2000, 5)  # 2 revolutions backward
+        utime.sleep(1)
+ 
+if __name__ == '__main__':
+    loop()
--- src/steppermotor.py
+++ src/steppermotor.py
@@ -1,34 +1,22 @@
-import machine
+from machine import Pin
 import utime
 
 class StepperMotor:
-    def __init__(self, steps_per_rev, pin1, pin2, pin3, pin4):
-        self.steps_per_rev = steps_per_rev
-        self.pins = [machine.Pin(pin, machine.Pin.OUT) for pin in [pin1, pin2, pin3, pin4]]
-        self.step_sequence = [
-            [1, 0, 0, 1],
-            [1, 0, 0, 0],
-            [1, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 1, 0],
-            [0, 0, 1, 0],
-            [0, 0, 1, 1],
-            [0, 0, 0, 1]
-        ]
-        self.step_count = len(self.step_sequence)
-        self.step_index = 0
-        self.delay = 0
-
-    def set_speed(self, speed):
-        # Speed is given as a percentage of the maximum speed
-        # We convert it to a delay in milliseconds between steps
-        self.delay = (1 - speed / 100) * 0.01
-
-    def step(self, steps):
-        for _ in range(int(steps)):
-            for pin, value in zip(self.pins, self.step_sequence[self.step_index]):
-                pin.value(value)
-            utime.sleep(self.delay)
-            self.step_index += 1
-            if self.step_index == self.step_count:
-                self.step_index = 0
+    def __init__(self, dir_pin, step_pin):
+        self.dir_pin = Pin(dir_pin, Pin.OUT)
+        self.step_pin = Pin(step_pin, Pin.OUT)
+        self.position = 0
+ 
+    def move(self, steps, delay, accel):
+        self.dir_pin.value(0 if steps > 0 else 1)
+        steps = abs(steps)
+        for i in range(steps):
+            self.step_pin.value(1)
+            utime.sleep_us(delay)
+            self.step_pin.value(0)
+            utime.sleep_us(delay)
+            if i < steps // 2 and delay > 100:
+                delay -= accel
+            elif i >= steps // 2 and delay < 2000:
+                delay += accel
+        self.position += steps if steps > 0 else -steps
