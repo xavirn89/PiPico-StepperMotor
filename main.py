a4ce43f41b440ba185af834bb18d461f8819adef
XaviRN <xavirn.me@gmail.com>
2024-03-29 15:30:34 +0100

    test: switch direction 1

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+from machine import Pin
 import utime
 from src.steppermotor import StepperMotor
 
 step_pin = 17
 dir_pin = 16
 stepper = StepperMotor(dir_pin, step_pin)
+switch_pin = Pin(14, Pin.IN, Pin.PULL_DOWN)
  
 def loop():
     while True:
-        stepper.move(600, 2000, 5)  # 2 revolutions forward
-        utime.sleep(1)
-        stepper.move(-600, 2000, 5)  # 2 revolutions backward
+        switch_state = switch_pin.value()
+        if switch_state:
+            stepper.move(600, 2000, 5)  # 2 revolutions forward
+        else:
+            stepper.move(-600, 2000, 5)  # 2 revolutions backward
         utime.sleep(1)
  
 if __name__ == '__main__':
